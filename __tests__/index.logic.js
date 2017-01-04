24ad2e57c88ac76e1100995f32d1bc13af682d86
Pekka Astala <pekka.astala@gmail.com>
2017-01-04 22:43:43 +0200

    Reorganised and extended tests

--- __tests__/index.logic.js
+++ __tests__/index.logic.js
@@ -1,48 +1,109 @@
 import Unit from "../src/gameLogic/Unit"
 import Player from "../src/gameLogic/Player"
 import MovementPattern from "../src/gameLogic/MovementPattern"
-import BoardState from "../src/gameLogic/BoardState"
+import CombatDetection from "../src/gameLogic/CombatDetection"
 
-describe("Unit", () => {
-  it("can be created", () => {
-    const unit = createUnit(0.1, 1.0)
-    expect(unit.getLocation()).toEqual(0.1)
+describe("Movement Pattern", () => {
+
+  it ("can be created", () => {
+    const pattern = new MovementPattern(0.0, 1.0)
+    expect(pattern instanceof MovementPattern)
   })
 
-  it("can advance forward", () => {
-    const unit = createUnit(0.1, 1.0)
-    unit.advance()
-    expect(unit.getLocation()).toEqual(0.11)
+  it ("steps forward in 0.01 increments", () => {
+    const pattern = new MovementPattern(0.0, 1.0)
+    pattern.step()
+    expect(pattern.getCurrentLocation() === 0.01)
   })
 
-  it("can advance backward", () => {
-    const unit = createUnit(1.0, 0.1)
-    unit.advance()
-    expect(unit.getLocation()).toEqual(0.99)
+  it ("steps backward in 0.01 increments", () => {
+    const pattern = new MovementPattern(1.0, 0.0)
+    pattern.step()
+    expect(pattern.getCurrentLocation() === 0.99)
   })
 
-  it("it dies when colliding with another unit", () => {
-
-    const player = createUnit(0.50, 0.51)
-    const enemy = createUnit(0.52, 0.51)
-
-    const board = new BoardState()
-    board.addUnit(player)
-    board.addUnit(enemy)
-
-    expect(player.isAlive()).toEqual(true)
-    expect(enemy.isAlive()).toEqual(true)
-
-    board.advanceUnits()
-    board.killCombattingUnits()
-
-    expect(player.isAlive()).toEqual(false)
-    expect(enemy.isAlive()).toEqual(false)
-  })
 })
 
-function createUnit(start, target) {
-  const plr = new Player()
-  const ptrn = new MovementPattern(start, target)
-  return new Unit(plr, ptrn)
-}
+describe("Unit", () => {
+
+  beforeEach(() => {
+    this.player = new Player("red", 0.0)
+    this.pattern = new MovementPattern(0.0, 1.0)
+  })
+
+  it ("can be created", () => {
+    const unit = new Unit(this.player, this.pattern)
+    expect(unit instanceof Unit)
+  })
+
+  it ("has location based on movement pattern", () => {
+    const unit = new Unit(this.player, this.pattern)
+    this.pattern.step()
+    expect(unit.getLocation() === this.pattern.getCurrentLocation())
+  })
+
+})
+
+describe("Combat Detection", () => {
+
+  beforeAll(() => {
+    this.playerA = new Player("#000000", 0.0)
+    this.playerB = new Player("#FFFFFF", 1.0)
+    this.pattern0 = new MovementPattern(0.00, 0.00)
+    this.pattern1 = new MovementPattern(0.01, 0.01)
+    this.pattern2 = new MovementPattern(0.02, 0.02)
+    this.pattern3 = new MovementPattern(0.03, 0.03)
+  })
+
+  it ("detects no combat if there are no units", () => {
+    const detector = new CombatDetection([])
+    expect (detector.getCombats().length === 0)
+  })
+
+  it ("detects no combat if there is only one unit", () => {
+    const units = [
+      new Unit(this.playerA, this.pattern0)
+    ]
+    const detector = new CombatDetection(units)
+    expect (detector.getCombats().length === 0)
+  })
+
+  it ("detects no combat if there are only one player's units", () => {
+    const units = [
+      new Unit(this.playerA, this.pattern0),
+      new Unit(this.playerA, this.pattern1)
+    ]
+    const detector = new CombatDetection(units)
+    expect (detector.getCombats().length === 0)
+  })
+
+  it ("detects no combat if hostile units are far from each other", () => {
+    const units = [
+      new Unit(this.playerA, this.pattern0),
+      new Unit(this.playerB, this.pattern3)
+    ]
+    const detector = new CombatDetection(units)
+    expect (detector.getCombats().length === 0)
+  })
+
+  it ("detects combat if two hostile units are adjacent", () => {
+    const units = [
+      new Unit(this.playerA, this.pattern1),
+      new Unit(this.playerB, this.pattern2)
+    ]
+    const detector = new CombatDetection(units)
+    expect (detector.getCombats().length === 1)
+  })
+
+  it ("detects multiple combats from multiple adjacent hostile pairs", () => {
+    const units = [
+      new Unit(this.playerA, this.pattern0),
+      new Unit(this.playerB, this.pattern1),
+      new Unit(this.playerB, this.pattern2),
+      new Unit(this.playerA, this.pattern3)
+    ]
+    const detector = new CombatDetection(units)
+    expect (detector.getCombats().length === 2)
+  })
+
+})
